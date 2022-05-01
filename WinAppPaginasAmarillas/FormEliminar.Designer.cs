b0ca397580645018f0c99738bad80e85d7befd61
AngelMelendres <78892684+AngelMelendres@users.noreply.github.com>
2022-05-01 01:28:41 -0500

    act

--- WinAppPaginasAmarillas/FormEliminar.Designer.cs
+++ WinAppPaginasAmarillas/FormEliminar.Designer.cs
@@ -29,91 +29,154 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.dgvListadoServicios = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoServicios)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 322);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(685, 488);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "CERRAR";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(271, 225);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 22);
-            this.textBox3.TabIndex = 7;
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(271, 162);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(203, 115);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(271, 99);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(203, 80);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(229, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(275, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.Size = new System.Drawing.Size(242, 13);
             this.label1.TabIndex = 9;
-            this.label1.Text = "ELIMINAR";
+            this.label1.Text = "ELIMINAR SERVICIO DE PAGINAS AMARILLAS";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 105);
+            this.label2.Location = new System.Drawing.Point(68, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 16);
+            this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 10;
-            this.label2.Text = "Nombre de la publicación";
+            this.label2.Text = "Buscar por ID";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(90, 165);
+            this.label4.Location = new System.Drawing.Point(68, 118);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(142, 16);
+            this.label4.Size = new System.Drawing.Size(83, 13);
             this.label4.TabIndex = 12;
-            this.label4.Text = "Código de publicación";
+            this.label4.Text = "Buscar por titulo";
+            // 
+            // dgvListadoServicios
+            // 
+            this.dgvListadoServicios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoServicios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvListadoServicios.Location = new System.Drawing.Point(7, 183);
+            this.dgvListadoServicios.Name = "dgvListadoServicios";
+            this.dgvListadoServicios.RowHeadersWidth = 51;
+            this.dgvListadoServicios.Size = new System.Drawing.Size(769, 242);
+            this.dgvListadoServicios.TabIndex = 13;
+            this.dgvListadoServicios.DoubleClick += new System.EventHandler(this.dgvListadoServicios_DoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "TITULO";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PROPIETARIO";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "DESCRIPCION";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "TELEFONO";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "TARJETA";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "ELIMINAR";
+            this.Column7.Name = "Column7";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(405, 136);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // FormEliminar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 452);
+            this.ClientSize = new System.Drawing.Size(788, 523);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dgvListadoServicios);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormEliminar";
             this.Text = "FormEliminar";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoServicios)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,11 +185,19 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvListadoServicios;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
--- WinAppPaginasAmarillas/FormEliminar.cs
+++ WinAppPaginasAmarillas/FormEliminar.cs
@@ -12,9 +12,45 @@ namespace WinAppPaginasAmarillas
 {
     public partial class FormEliminar : Form
     {
+        ClassPaginasAmarillas objPagina = new ClassPaginasAmarillas();
+        private int edit_indice = -1;
+
         public FormEliminar()
         {
             InitializeComponent();
+        }
+        private void actualizarGrid()
+        {
+
+            dgvListadoServicios.DataSource = null;
+
+            dgvListadoServicios.AutoGenerateColumns = true;
+            dgvListadoServicios.DataSource = objPagina.mostrarTodosServicios();
+
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (edit_indice > -1) //verifica si hay un índice seleccionado
+            {
+                objPagina.eliminarServicio(edit_indice);
+                edit_indice = -1; //resetea variable a -1
+                actualizarGrid();
+            }
+            else
+            {
+                MessageBox.Show("Dar doble click sobre elemento para seleccionar y borrar ");
+            }
+
+        }
+
+        private void dgvListadoServicios_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow selected = dgvListadoServicios.SelectedRows[0];
+            int posicion = dgvListadoServicios.Rows.IndexOf(selected);
+            edit_indice = posicion;
+            cServicio servicio = objPagina.mostrarTodosServicios()[posicion];
+
+
         }
     }
 }
